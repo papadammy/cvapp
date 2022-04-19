b6f79ac671bf4949dd1b47e995504e3ae9fb4815
papadammy <papadammy4real@gmail.com>
2022-04-19 09:09:51 +0100

    Updates

---
+++ precache-manifest.8340f158602ea608502cf059d26877bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a66255ed188482f1beb4",
+    "url": "%7Bpapadammy%7D.github.io/static/js/main.a66255ed.chunk.js"
+  },
+  {
+    "revision": "4092d002d2ec73685048",
+    "url": "%7Bpapadammy%7D.github.io/static/js/1.4092d002.chunk.js"
+  },
+  {
+    "revision": "eb89515369c9a507d954",
+    "url": "%7Bpapadammy%7D.github.io/static/js/runtime~main.eb895153.js"
+  },
+  {
+    "revision": "afd256cbcfe5efb6828a3b9664e06113",
+    "url": "%7Bpapadammy%7D.github.io/index.html"
+  }
+];
